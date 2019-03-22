0663e48a59af4ffd73627d4b8e87d08e950012db
Saussereau Dominique <domsau2@yahoo.fr>
2019-03-22 14:39:37 +0100

    errPasPremierGroupe, présentation.

--- main.cpp
+++ main.cpp
@@ -11,28 +11,36 @@ string tolow(string a) // https://stackoverflow.com/questions/11635/case-insensi
     return a;
 }
 
+void errPasPremierGroupe(){
+cerr << "Votre verbe n\'est pas du premier groupe ! Recommencez !" << endl;
+    
+}
+
 int main()
 {
     string verb="";
+
 entrer:
     cout << "Entrez votre verbe du premier groupe : " << endl;
     getline(cin, verb);
     verb = tolow(verb);
 
 if (verb == "aller"){
+    errPasPremierGroupe()
     goto entrer;
 }
 if (verb.substr(verb.size() - 2,verb.size()) != "er"){
+    errPasPremierGroupe()
     goto entrer;
 }
 
     verb = verb.substr(0,verb.size() - 2);
 
-cout<<"je "<<verb<<"e"<<endl;
-cout<<"tu "<<verb<<"es"<<endl;
-cout<<"il "<<verb<<"e"<<endl;
-cout<<"nous "<<verb<<"ons"<<endl;
-cout<<"vous "<<verb<<"ez"<<endl;
-cout<<"ils "<<verb<<"ent"<<endl;
+cout<<"Je "<<verb<<"e."<<endl;
+cout<<"Tu "<<verb<<"es."<<endl;
+cout<<"Il "<<verb<<"e."<<endl;
+cout<<"Nous "<<verb<<"ons."<<endl;
+cout<<"Vous "<<verb<<"ez."<<endl;
+cout<<"Ils "<<verb<<"ent."<<endl;
     return 0;
 }

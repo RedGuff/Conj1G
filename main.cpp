936b4aea2df032678daae37d59258157f8945ef1
Saussereau Dominique <domsau2@yahoo.fr>
2019-07-29 17:22:13 +0200

    Verbes pronominaux ok, (pas encore de gestion des accents).

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+bool accent = false; // Se lever !
+
 using namespace std;
 string tolow(string a) // https://stackoverflow.com/questions/11635/case-insensitive-string-comparison-in-c
 {
@@ -11,36 +13,47 @@ string tolow(string a) // https://stackoverflow.com/questions/11635/case-insensi
     return a;
 }
 
-void errPasPremierGroupe(){
-cerr << "Votre verbe n\'est pas du premier groupe ! Recommencez !" << endl;
-    
+void errPasPremierGroupe()
+{
+    cerr << "Votre verbe n\'est pas du premier groupe ! Recommencez !" << endl;
 }
 
 int main()
 {
     string verb="";
-
 entrer:
     cout << "Entrez votre verbe du premier groupe : " << endl;
     getline(cin, verb);
     verb = tolow(verb);
-
-if (verb == "aller"){
-    errPasPremierGroupe()
-    goto entrer;
-}
-if (verb.substr(verb.size() - 2,verb.size()) != "er"){
-    errPasPremierGroupe()
-    goto entrer;
-}
-
+    if (verb == "aller")
+    {
+        errPasPremierGroupe();
+        goto entrer;
+    }
+    if (verb.substr(verb.size() - 2,verb.size()) != "er")
+    {
+        errPasPremierGroupe();
+        goto entrer;
+    }
     verb = verb.substr(0,verb.size() - 2);
-
-cout<<"Je "<<verb<<"e."<<endl;
-cout<<"Tu "<<verb<<"es."<<endl;
-cout<<"Il "<<verb<<"e."<<endl;
-cout<<"Nous "<<verb<<"ons."<<endl;
-cout<<"Vous "<<verb<<"ez."<<endl;
-cout<<"Ils "<<verb<<"ent."<<endl;
+    if ((verb.substr(0,3) == "se "))
+    {
+        verb = verb.substr(3,verb.size());
+        cout<<"Je me "<<verb<<"e."<<endl;
+        cout<<"Tu le "<<verb<<"es."<<endl;
+        cout<<"Il se "<<verb<<"e."<<endl;
+        cout<<"Nous nous "<<verb<<"ons."<<endl;
+        cout<<"Vous vous "<<verb<<"ez."<<endl;
+        cout<<"Ils se "<<verb<<"ent."<<endl;
+    }
+    else
+    {
+        cout<<"Je "<<verb<<"e."<<endl;
+        cout<<"Tu "<<verb<<"es."<<endl;
+        cout<<"Il "<<verb<<"e."<<endl;
+        cout<<"Nous "<<verb<<"ons."<<endl;
+        cout<<"Vous "<<verb<<"ez."<<endl;
+        cout<<"Ils "<<verb<<"ent."<<endl;
+    }
     return 0;
 }

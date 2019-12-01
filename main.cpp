9753c669c72dd02a2e51185738f7eb3f2b8cc1de
Saussereau Dominique <domsau2@yahoo.fr>
2019-12-01 14:36:11 +0100

    Capitales, lien, iel...

--- main.cpp
+++ main.cpp
@@ -4,56 +4,57 @@
 bool accent = false; // Se lever !
 
 using namespace std;
-string tolow(string a) // https://stackoverflow.com/questions/11635/case-insensitive-string-comparison-in-c
-{
-    for(unsigned int i=0; i<a.length(); i++)
-    {
-        a[i]=tolower(a[i]);
-    }
+string toUp ( string a ) { // https://stackoverflow.com/questions/11635/case-insensitive-string-comparison-in-c
+    for ( unsigned int i=0; i<a.length(); i++ ) {
+        a[i]=toupper ( a[i] );
+        }
+
     return a;
-}
+    }
 
-void errPasPremierGroupe()
-{
+void errPasPremierGroupe() {
     cerr << "Votre verbe n\'est pas du premier groupe ! Recommencez !" << endl;
-}
+    }
 
-int main()
-{
+int main() {
     string verb="";
 entrer:
+    cout << "Les accents et autres diacritiques ne sont pas rendus." << endl;
+    cout << "C\'est pour cela que j\'affiche la congaison en lettres capitales." << endl;
+    cout << "https://fr.wikipedia.org/wiki/Capitale_et_majuscule#Diff%C3%A9rence_entre_les_capitales_et_les_majuscules" << endl;
     cout << "Entrez votre verbe du premier groupe : " << endl;
-    getline(cin, verb);
-    verb = tolow(verb);
-    if (verb == "aller")
-    {
+    getline ( cin, verb );
+    verb = toUp ( verb );
+
+    if ( verb == "ALLER" ) {
         errPasPremierGroupe();
         goto entrer;
-    }
-    if (verb.substr(verb.size() - 2,verb.size()) != "er")
-    {
+        }
+
+    if ( verb.substr ( verb.size() - 2,verb.size() ) != "ER" ) {
         errPasPremierGroupe();
         goto entrer;
-    }
-    verb = verb.substr(0,verb.size() - 2);
-    if ((verb.substr(0,3) == "se "))
-    {
-        verb = verb.substr(3,verb.size());
-        cout<<"Je me "<<verb<<"e."<<endl;
-        cout<<"Tu le "<<verb<<"es."<<endl;
-        cout<<"Il se "<<verb<<"e."<<endl;
-        cout<<"Nous nous "<<verb<<"ons."<<endl;
-        cout<<"Vous vous "<<verb<<"ez."<<endl;
-        cout<<"Ils se "<<verb<<"ent."<<endl;
-    }
-    else
-    {
-        cout<<"Je "<<verb<<"e."<<endl;
-        cout<<"Tu "<<verb<<"es."<<endl;
-        cout<<"Il "<<verb<<"e."<<endl;
-        cout<<"Nous "<<verb<<"ons."<<endl;
-        cout<<"Vous "<<verb<<"ez."<<endl;
-        cout<<"Ils "<<verb<<"ent."<<endl;
-    }
+        }
+
+    verb = verb.substr ( 0,verb.size() - 2 );
+
+    if ( ( verb.substr ( 0,3 ) == "SE " ) ) {
+        verb = verb.substr ( 3,verb.size() );
+        cout<<"JE ME "<<verb<<"E."<<endl;
+        cout<<"TU TE "<<verb<<"ES."<<endl;
+        cout<<"IL/ELLE/IEL/IELLE/ON SE "<<verb<<"E."<<endl;
+        cout<<"NOUS NOUS "<<verb<<"ONS."<<endl;
+        cout<<"VOUS VOUS "<<verb<<"EZ."<<endl;
+        cout<<"ILS/ELLES/IELS/IELLES SE "<<verb<<"ENT."<<endl;
+        }
+    else {
+        cout<<"JE "<<verb<<"E."<<endl;
+        cout<<"TU "<<verb<<"ES."<<endl;
+        cout<<"IL/ELLE/IEL/IELLE "<<verb<<"E."<<endl;
+        cout<<"NOUS "<<verb<<"ONS."<<endl;
+        cout<<"VOUS "<<verb<<"EZ."<<endl;
+        cout<<"ILS/ELLES/IELS/IELLES "<<verb<<"ENT."<<endl;
+        }
+
     return 0;
-}
+    }
